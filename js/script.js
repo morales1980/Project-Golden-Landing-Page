f6afc2b9fa779c7cc4d069469ab0f5b4d15efec3
Darek.Szotowicz <dariusz.szotowicz@protonmail.com>
2019-02-05 21:33:29 +0100

    add media queries

--- js/script.js
+++ js/script.js
@@ -1,17 +1,29 @@
 (function () {
   var mobileNav = document.querySelector('.mobile-nav');
   var navMenu = document.querySelector('header nav ul');
+  var jumboHeader1 = document.querySelector('#home h1');
+  var jumboHeader2 = document.querySelector('#home h2');
   function toggleClass() {
-    mobileNav.classList.toggle('open');
     showNavMenu();
+    mobileNav.classList.toggle('open');
   }
   function showNavMenu() {
-    if(navMenu.style.display === 'none') {
-      navMenu.style.display = 'block';
+    if(navMenu.style.visibility === 'visible') {
+      navMenu.style.visibility = 'hidden';
+      navMenu.style.opacity = '0';
+      jumboHeader1.style.visibility = 'visible';
+      jumboHeader1.style.opacity = '1';
+      jumboHeader2.style.visibility = 'visible';
+      jumboHeader2.style.opacity = '1';
     } else {
-      navMenu.style.display = 'none';
+      navMenu.style.visibility = 'visible';
+      navMenu.style.opacity = '1';
+      jumboHeader1.style.visibility = 'hidden';
+      jumboHeader1.style.opacity = '0';
+      jumboHeader2.style.visibility = 'hidden';
+      jumboHeader2.style.opacity = '0';
     }
-
   }
+
   mobileNav.addEventListener('click', toggleClass);
 })();
